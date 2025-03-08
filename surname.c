6d6342958214d76d2c22bd3553fd98de548555b1
PIYUSH-22131034 <2024pietadpiyush041@poornima.org>
2025-03-08 09:34:27 +0000

    Add files via upload

---
+++ surname.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char forename[50], surname[50];
+    int year_of_birth;
+
+    
+    printf("Enter your forename: ");
+    scanf("%s", forename);
+
+    printf("Enter your surname: ");
+    scanf("%s", surname);
+
+    printf("Enter your year of birth: ");
+    scanf("%d", &year_of_birth);
+
+       printf("\nYou entered:\n");
+    printf("Forename: %s\n", forename);
+    printf("Surname: %s\n", surname);
+    printf("Year of birth: %d\n", year_of_birth);
+
+    return 0;
+}
+
